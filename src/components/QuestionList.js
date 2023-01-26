088395c740ecf25bb269016dd836612492b003ed
Loise <loise.kimani@student.moringaschool.com>
2023-01-26 11:21:39 +0300

    Changed the fetch calls to async syntax for readability

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,27 +1,16 @@
 import React, { useEffect, useState } from "react";
-import QuestionItem from "./QuestionItem";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function questionList() {
   const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:4000/questions")
-      .then((r) => r.json())
+      .then((response) => response.json())
       .then((questions) => {
         setQuestions(questions);
       });
   }, []);
-
-  function handleDeleteClick(id) {
-    fetch(`http://localhost:4000/questions/${id}`, {
-      method: "DELETE",
-    })
-      .then((r) => r.json())
-      .then(() => {
-        const updatedQuestions = questions.filter((q) => q.id !== id);
-        setQuestions(updatedQuestions);
-      });
-  }
 
   function handleAnswerChange(id, correctIndex) {
     fetch(`http://localhost:4000/questions/${id}`, {
@@ -31,20 +20,31 @@ function QuestionList() {
       },
       body: JSON.stringify({ correctIndex }),
     })
-      .then((r) => r.json())
+      .then((response) => response.json())
       .then((updatedQuestion) => {
-        const updatedQuestions = questions.map((q) => {
-          if (q.id === updatedQuestion.id) return updatedQuestion;
-          return q;
+        const updatedQuestions = questions.map((e) => {
+          if (e.id === updatedQuestion.id) return updatedQuestion;
+          return e;
         });
         setQuestions(updatedQuestions);
       });
   }
 
-  const questionItems = questions.map((q) => (
+  function handleDeleteClick(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        const updatedQuestions = questions.filter((e) => e.id !== id);
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  const questionItems = questions.map((e) => (
     <QuestionItem
-      key={q.id}
-      question={q}
+      key={e.id}
+      question={e}
       onDeleteClick={handleDeleteClick}
       onAnswerChange={handleAnswerChange}
     />
@@ -52,10 +52,10 @@ function QuestionList() {
 
   return (
     <section>
-      <h1>Quiz Questions</h1>
+      <h1>Quiz questions</h1>
       <ul>{questionItems}</ul>
     </section>
   );
 }
 
-export default QuestionList;
+export default questionList;
